29293b7f086d21d044f53aa4e295fd018883db6a
kafkaesc <jared.a.hettinger@gmail.com>
2023-06-12 19:57:08 -0500

    Adding element components and dashboard components to pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,16 @@
+import A from '@/elements/A';
+import H1 from '@/elements/H1';
+import Link from '@/elements/Link';
+import P from '@/elements/P';
+
 export default function About() {
-	return <h1 className="text-4xl text-center">About</h1>;
+	return (
+		<>
+			<H1 className="text-4xl text-center">About</H1>
+			<P className="text-center">Built by Jared Hettinger</P>
+			<P className="text-center">
+				Return <Link href="/">home</Link>
+			</P>
+		</>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import P from '@/elements/P';
+import H1 from '@/elements/H1';
+import Link from '@/elements/Link';
 import DashFull from '@/components/DashFull';
 import DashHalf from '@/components/DashHalf';
 import DashQuarter from '@/components/DashQuarter';
@@ -5,13 +8,16 @@ import DashQuarter from '@/components/DashQuarter';
 export default function Home() {
 	return (
 		<>
-			<h1 className="text-4xl text-center">Home</h1>
+			<H1 className="text-4xl text-center">Home</H1>
 			<DashFull title="Wide Data">Hello world</DashFull>
 			<DashHalf title="Half Data 1">Abc</DashHalf>
 			<DashHalf title="Half Data 2">Def</DashHalf>
 			<DashQuarter title="Q1">Hello World</DashQuarter>
 			<DashQuarter title="Q2">Hola el Mundo</DashQuarter>
 			<DashHalf title="Half Data 2">Def</DashHalf>
+			<P className="text-center">
+				<Link href="/About">About</Link>
+			</P>
 		</>
 	);
 }
