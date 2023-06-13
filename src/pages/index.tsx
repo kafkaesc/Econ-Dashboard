cf26c6149cda4c4e47969372d274d0edd52ebb2d
kafkaesc <jared.a.hettinger@gmail.com>
2023-06-12 19:06:55 -0500

    Adding dash boxes to the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
+import DashFull from '@/components/DashFull';
+import DashHalf from '@/components/DashHalf';
+import DashQuarter from '@/components/DashQuarter';
+
 export default function Home() {
-	return <h1 className="text-4xl text-center">Home</h1>;
+	return (
+		<>
+			<h1 className="text-4xl text-center">Home</h1>
+			<DashFull title="Wide Data">Hello world</DashFull>
+			<DashHalf title="Half Data 1">Abc</DashHalf>
+			<DashHalf title="Half Data 2">Def</DashHalf>
+			<DashQuarter title="Q1">Hello World</DashQuarter>
+			<DashQuarter title="Q2">Hola el Mundo</DashQuarter>
+			<DashHalf title="Half Data 2">Def</DashHalf>
+		</>
+	);
 }
