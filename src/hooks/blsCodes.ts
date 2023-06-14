68b75967bdca6ede49366b47e3c327f61c41694c
kafkaesc <jared.a.hettinger@gmail.com>
2023-06-14 09:16:15 -0500

    Consolidating BLS API code enums

--- src/hooks/blsCodes.ts
+++ src/hooks/blsCodes.ts
@@ -47,3 +47,32 @@ export enum AreaCodes {
 	UrbanHawaii = 'S49F',
 	UrbanAlaska = 'S49G',
 }
+
+// Enum is non-exhaustive, codes are from the BLS API documentation at:
+// https://download.bls.gov/pub/time.series/cu/cu.item
+export enum CpiItemCodes {
+	AllItems = 'SA0',
+	PurchasingPowerOfTheConsumerDollar = 'SA0R',
+	MensAndBoysApparel = 'SAA1',
+	WomensAndGirlsApparel = 'SAA2',
+	Commodities = 'SAC',
+	EnergyCommodities = 'SACE',
+	FoodAndBeverages = 'SAF',
+	Food = 'SAF1',
+	Housing = 'SAH',
+	Shelter = 'SAH1',
+	FuelsAndUtilities = 'SAH2',
+	HouseholdEnergy = 'SAH21',
+	MedicalCare = 'SAM',
+	JewelryAndWatches = 'SEAG',
+	Jewelry = 'SEAG01',
+	Watches = 'SEAG02',
+	Coffee = 'SEFP01',
+}
+
+// This should be exhaustive, but not totally certain
+// https://www.bls.gov/help/def/cu.htm
+export enum Periodicity {
+	Monthly = 'R',
+	SemiAnnual = 'S',
+}
--- src/hooks/blsPeriodicity.ts
+++
@@ -1,6 +0,0 @@
-// This should be exhaustive, but not totally certain
-// https://www.bls.gov/help/def/cu.htm
-export enum Periodicity {
-	Monthly = 'R',
-	SemiAnnual = 'S',
-}
