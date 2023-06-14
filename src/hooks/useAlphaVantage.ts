3e7da2897029e1f1f9713ae6d5984f6a50aaef10
kafkaesc <jared.a.hettinger@gmail.com>
2023-06-14 11:24:55 -0500

    Adding getTimeSeriesIntradayBySymbol to useAlphaVantage

--- src/hooks/useAlphaVantage.ts
+++ src/hooks/useAlphaVantage.ts
@@ -1,11 +1,18 @@
 // Alpha Vantage API Documentation: https://www.alphavantage.co/documentation/
-
 const apiKey = process.env.NEXT_PUBLIC_ALPHA_VANTAGE_API_KEY;
 
 export function useAlphaVantage() {
+	async function getTimeSeriesIntradayBySymbol(symbol: string) {
+		return fetch(
+			`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${apiKey}`
+		)
+			.then((res) => res.json())
+			.then((jsonData) => jsonData);
+	}
+
 	function logData() {
 		console.log(apiKey);
 	}
 
-	return { logData };
+	return { getTimeSeriesIntradayBySymbol, logData };
 }
